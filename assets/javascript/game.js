f093b8d6f2f90de57ac949bc96ad1325cb5cbd38
T4T4el7 <ochoaarmando13@gmail.com>
2018-12-06 13:03:14 -0800

    found some premade code to help outline the basics of setting the game start. Set a random number generator to choose a secret word from the word bank. set variables for the word i will be matching as well as the arrays for letters already used that are both erronious and correct to the guessing as well as finsing a method to push them to the designated areas on the page that i want them to go.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,67 @@
-var a = "hello world";
-console.log(a);
+
+var words = ["fedddy" , "jason" , "pinhead" , "leatherface"]; //the word bank
+
+ var sWord ;
+
+
+// var remainingLetters = sWord.length ; 
+// console.log(remainingLetters);
+
+var wins = 0;
+var losses = 0;
+var lives = 0;
+var maxTries = 9;
+
+var tWord = [] ;  //the word we are building to try and match the random word
+var used = [] ; //array that stores the already used words
+
+function start() {
+    lives = maxTries;
+    console.log(lives);
+
+    var sWord = words[Math.floor(Math.random() * words.length)]; //random word to be guessed
+    console.log(sWord);
+
+    used = [];
+    tWord = [];
+
+    for( var i = 0; i < sWord.length; i++) {
+        tWord.push("_");
+        console.log(tWord);
+    }
+
+    game();
+};
+
+function game(){
+    document.getElementById("wins").innerText= wins;
+    var tWordText= "";
+    for(var j = 0; j <tWord.length; j++) {
+        tWordText += tWord[j];
+        console.log(tWordText);
+    }
+  
+    document.getElementById("answer").innerText= tWordText;
+    document.getElementById("lives").innerText= lives;
+    document.getElementById("used").innerText= used;
+
+};
+
+
+// document.onkeyup = function(event) {    
+//     var guess = event.key ;             //defines the letter being guessed via a key press
+//     console.log(guess) ;
+//     if ( guess === secretWord.charAt(0)) {
+//     console.log("its a match");
+//     document.getElementById("answer").append(guess);
+//     } else {
+//         document.getElementById("used").append(guess);
+//     }
+   
+
+// };
+
+
+
+
+
