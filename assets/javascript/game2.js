263008a33bfa4b57a04b3b8bb30f81dfd4ca8ffe
T4T4el7 <ochoaarmando13@gmail.com>
2018-12-08 13:33:08 -0800

    well i managed to get the scoring system and the reset system working for the pyschic game

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,24 +1,53 @@
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-var sLet = letters [Math.floor(Math.random() * letters.length)]; //random letter to be guessed
-    console.log(sLet);
-
+var sLett ;
+var guess;
 var wins = 0;
 var losses = 0;
 var lives = 0;
 var maxTries = 9;
 
 
-function start() {
+function reset() {
+    var sLet = letters [Math.floor(Math.random() * letters.length)]; //random letter to be guessed
+    console.log(sLet);
     lives = maxTries;
-    wins = [];
-    losses = [];
-
-}
+    sLett = sLet;
+};
 
 function letsPlay(){
+    
     document.onkeyup = function(event) {    
      var guess = event.key ;             //defines the letter being guessed via a key press
         console.log(guess) ;
-    }
+        document.getElementById("used").append(guess);  
+
+        if( guess === sLett) {
+            console.log("cool");
+            alert("Yay its a match");
+            wins++;
+            console.log(wins); 
+            document.getElementById("tWins").innerText = wins;
+            reset();
+            
+         
+        } else if ( guess !== sLett) {
+            console.log("oh no");
+            lives--;
+            console.log(lives);
+            document.getElementById("tLives").innerText = lives;
+
+            if ( lives === 0 ) {
+                alert("oh no you lost");
+                losses++;
+                console.log(losses);
+                document.getElementById("tLosses").innerText = losses;
+                reset();
+            };
+
+        };
+    }; 
 };
+
+
+
